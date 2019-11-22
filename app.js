afbacd805181b48013d5b43bc610a799a629604e
Mohamed Noushad <mohamednoushad@attinadsoftware.com>
2019-11-22 11:36:17 +0530

    modified methods

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  return res.send('Received a GET HTTP method');
+  return res.send('GET method');
 });
+
 app.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method');
+  return res.send('POST method');
 });
+
 app.put('/', (req, res) => {
-  return res.send('Received a PUT HTTP method');
+  return res.send('PUT method');
 });
+
 app.delete('/', (req, res) => {
-  return res.send('Received a DELETE HTTP method');
+  return res.send('DELETE method');
 });
+
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
 );
